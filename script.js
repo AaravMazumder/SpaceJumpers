1244d14b864491bdeec7d95fc97da50eb71d050a
Aarav Mazumder <aarav@Biplabs-MacBook-Pro.local>
2020-10-29 21:48:44 -0400

    one

---
+++ script.js
@@ -0,0 +1,11 @@
+document.onkeydown = function(e){
+    console.log("key code is: ", e.keyCode)
+    if(e.keyCode==38){
+        guy = document.querySelector('.guy');
+        guy.classList.add('animateGuy');
+        setTimeout(() => {
+           guy.classList.remove('animateGuy') 
+        }, 1000);
+    }
+}
+ 
